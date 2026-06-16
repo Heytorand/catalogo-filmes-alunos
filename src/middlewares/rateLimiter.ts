b3b04b09a0e9852e7596639478480c4b717c0f77
Heytor <heytorandrademedeiros2009@gmail.com>
2026-06-16 17:56:16 -0300

    tudo certo

--- src/middlewares/rateLimiter.ts
+++ src/middlewares/rateLimiter.ts
@@ -1,18 +1,7 @@
-import { Request, Response, NextFunction } from "express";
+import rateLimit from "express-rate-limit";
 
-export function sanitizar(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  for (const campo in req.body) {
-    if (typeof req.body[campo] === "string") {
-      req.body[campo] = req.body[campo]
-        .trim()
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;");
-    }
-  }
-
-  next();
-}
+export const limiterGeral = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Muitas requisições. Tente novamente mais tarde.",
+});
