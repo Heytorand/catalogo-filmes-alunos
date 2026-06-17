8ce89a3cbecd574bc963f9c5217325b30aba4e4f
Heytor <heytorandrademedeiros2009@gmail.com>
2026-06-17 11:19:03 -0300

    salvando

--- src/routes/apiRoutes.ts
+++ src/routes/apiRoutes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { readFileSync, writeFileSync } from "fs";
 export const apiRoutes = Router();
 
+
 interface Filme { id:number; titulo:string; diretor:string; ano:number; genero:string; nota:number; }
 function carregar(): Filme[] { return JSON.parse(readFileSync("dados/filmes.json","utf-8")); }
 function salvar(f: Filme[]) { writeFileSync("dados/filmes.json", JSON.stringify(f,null,2)); }
